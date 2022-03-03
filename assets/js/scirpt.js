a518189c6b0c06024c0e3efd6ddbe6e664072bb2
Van Hung <vanhungnguyenbku@gmail.com>
2022-03-04 00:13:29 +0700

    .

--- assets/js/scirpt.js
+++ assets/js/scirpt.js
@@ -1,7 +1,25 @@
+// Sticky navbar
 $(window).on('scroll', function () {
 	if ($(window).scrollTop()) {
 		$('header').addClass('black')
 	} else {
 		$('header').removeClass('black')
 	}
+})
+
+// Swiper slider
+var swiper = new Swiper('.mySwiper', {
+	slidesPerView: 2,
+	spaceBetween: 20,
+	slidesPerGroup: 2,
+	loop: true,
+	loopFillGroupWithBlank: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	// navigation: {
+	// 	nextEl: '.swiper-button-next',
+	// 	prevEl: '.swiper-button-prev',
+	// },
 })
