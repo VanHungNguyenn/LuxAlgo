c379e5e4ba77a5b724cad93f949035516fb610bc
VanHungNguyen <vanhungnguyenbku@gmail.com>
2022-03-16 18:17:44 +0700

    .

--- assets/js/scirpt.js
+++ assets/js/scirpt.js
@@ -66,3 +66,55 @@ function showMenu() {
 function hideMenu() {
 	menu.style.top = '-100vh'
 }
+
+// user__dropdown
+const topPanelUserInfo = document.querySelector('.top-panel__user-info')
+const userDropdown = document.querySelector('.user__dropdown')
+topPanelUserInfo.addEventListener('click', () => {
+	userDropdown.classList.toggle('active')
+})
+
+// click outside of user__dropdown to hide it
+document.addEventListener('click', (e) => {
+	if (!e.target.closest('.top-panel__user-right')) {
+		userDropdown.classList.remove('active')
+	}
+})
+
+// Switch tabs
+function switchTab(tab) {
+	let i
+	const tabs = document.querySelectorAll('.subscription__tab-list')
+
+	for (i = 0; i < tabs.length; i++) {
+		tabs[i].style.display = 'none'
+	}
+
+	const tabsItem = document.querySelectorAll('.subscription__main-tab-item')
+	//toggle class active on tabs
+	for (i = 0; i < tabsItem.length; i++) {
+		tabsItem[i].classList.toggle('active')
+	}
+
+	document.getElementById(tab).style.display = 'flex'
+}
+
+// depositModal
+const modalDepositPaymentItems = document.querySelectorAll(
+	'.modal-deposit-payment-item'
+)
+
+for (let index = 0; index < modalDepositPaymentItems.length; index++) {
+	modalDepositPaymentItems[index].addEventListener('click', function () {
+		const modalDepositPaymentItem = document.querySelectorAll(
+			'.modal-deposit-payment-item'
+		)
+		for (let index = 0; index < modalDepositPaymentItem.length; index++) {
+			modalDepositPaymentItem[index].classList.remove('active')
+		}
+		this.classList.add('active')
+	})
+}
+
+const modalDepositCheckbox = document.querySelector('.modal-deposit-checkbox')
+const modalDepositButton = document.querySelector('.modal-deposit-button')
